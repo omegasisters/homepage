de271d445039ee4adc5b2d5392a7e4fbea141328
Tadao Iseki <saitoeku3@gmail.com>
2019-12-19 02:27:56 +0900

    Twitterアイコンを遅延読み込みするようにした

---
+++ assets/js/lazyload.js
@@ -0,0 +1,11 @@
+const observer = new IntersectionObserver((entries, observer) => {
+  entries.forEach(({ isIntersecting, target }) => {
+    if (!isIntersecting) return
+    target.src = target.dataset.src
+    observer.unobserve(target)
+  })
+})
+
+document.querySelectorAll('.lazy').forEach(item => {
+  observer.observe(item)
+})
