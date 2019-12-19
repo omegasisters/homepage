30bf1fef0664933be322ff479b5365aeb9f6b322
nzws <git@nzws.me>
2019-12-19 19:33:55 +0900

    ビューワーのスタイルを修正

--- preact_build/App.js
+++ preact_build/App.js
@@ -3,6 +3,7 @@ import { useEffect, useRef, useState } from "/homepage/web_modules/preact/hooks.
 import ThreeViewer from "./three/viewer.js";
 var App = function () {
     var _a = useState(0), progress = _a[0], setProgress = _a[1];
+    var _b = useState('otohime'), modelType = _b[0], setModelType = _b[1];
     var isLoading = progress > 0 && progress < 100;
     var divRef = useRef();
     var viewerRef = useRef(new ThreeViewer());
@@ -16,27 +17,31 @@ var App = function () {
         };
     }, []);
     var onOtohime = function () {
-        if (!isLoading)
+        if (!isLoading) {
             viewer.loadVrm(setProgress);
+            setModelType('otohime');
+        }
     };
     var onCuring = function () {
-        if (!isLoading)
+        if (!isLoading) {
             viewer.loadFbx(setProgress);
+            setModelType('curing');
+        }
     };
-    return (h("div", { style: { width: "100%" } },
-        h("p", null, "\u304A\u3068\u3072\u3081.vrm"),
-        h("a", { href: "https://3d.nicovideo.jp/works/td41391", target: "_blank", rel: "noopener noreferrer", style: { wordWrap: "break-word" } }, "https://3d.nicovideo.jp/works/td41391"),
-        h("p", null, "\u3046\u3093\u3061\u30AB\u30FC\u30EA\u30F3\u30B0"),
-        h("a", { href: "https://omesis-shop.booth.pm/items/1269953", target: "_blank", rel: "noopener noreferrer", style: { wordWrap: "break-word" } }, "https://omesis-shop.booth.pm/items/1269953"),
-        h("br", null),
-        h("br", null),
-        h("div", { style: { display: "flex", justifyContent: "center" } },
-            h("button", { onClick: onOtohime }, "\u304A\u3068\u3072\u3081"),
-            h("button", { onClick: onCuring }, "\u3046\u3093\u3061\u304B\u30FC\u308A\u3093\u3050")),
+    return (h("div", { className: "viewer" },
+        h("div", { className: "controller" },
+            h("button", { onClick: onOtohime, className: modelType === 'otohime' ? 'active' : '' }, "\u304A\u3068\u3072\u3081"),
+            h("button", { onClick: onCuring, className: modelType === 'curing' ? 'active' : '' }, "\u3046\u3093\u3061\u304B\u30FC\u308A\u3093\u3050")),
         isLoading && h("p", null,
             "Now Loading ",
             progress,
             "%"),
+        modelType === 'otohime' && (h("div", { className: "description" },
+            h("p", null, "\u304A\u3068\u3072\u3081.vrm"),
+            h("a", { href: "https://3d.nicovideo.jp/works/td41391", target: "_blank", rel: "noopener noreferrer" }, "3d.nicovideo.jp/works/td41391"))),
+        modelType === 'curing' && (h("div", { className: "description" },
+            h("p", null, "\u3046\u3093\u3061\u30AB\u30FC\u30EA\u30F3\u30B0"),
+            h("a", { href: "https://omesis-shop.booth.pm/items/1269953", target: "_blank", rel: "noopener noreferrer" }, "omesis-shop.booth.pm/items/1269953"))),
         h("div", { ref: divRef })));
 };
 export default App;
