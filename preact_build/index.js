2e1833f66e6202158b47917e4bcff11eb8844e56
shinyoshiaki <shinyoshiaki2012@gmail.com>
2019-12-19 17:10:24 +0900

    fix js

--- preact_build/index.js
+++ preact_build/index.js
@@ -1,6 +1,7 @@
-import { h, render } from "/web_modules/preact.js";
+import { h, render } from "../web_modules/preact.js";
+
 import App from "./App.js";
+
 var appMount = document.querySelector("#preact");
-if (appMount)
-    render(h(App, null), appMount);
+if (appMount) render(h(App, null), appMount);
 export default App;
