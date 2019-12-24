e2689b178a2b9a7c9ceffd68097f3789fb4190ad
Iwaide <iwaide@hoobby.net>
2019-12-24 23:59:39 +0900

    Add mobile header

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,6 +1,21 @@
 document.onscroll = () => {
   const windowTop = document.documentElement.scrollTop;
-  const element = document.getElementById('header');
+  const element = document.getElementsByTagName('header')[0];
 
-  element.className = windowTop > 80 ? 'scrolled' : '';
+  if (windowTop > 80) {
+    element.classList.add('scrolled');
+  } else {
+    element.classList.remove('scrolled');
+  }
 };
+$(document).ready(function() {
+  $('.drawer').drawer();
+
+  $('.drawer-toggle').click(function() {
+    $('header .menu-icon').toggleClass('opened');
+  });
+
+  $('.drawer-menu-item').click(function() {
+    $('.drawer').drawer('toggle');
+  });
+});
