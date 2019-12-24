ceb14c6fdd4af364589f47add1d933517f335c2c
shinyoshiaki <shinyoshiaki2012@gmail.com>
2019-12-24 16:10:48 +0900

    fix animation

--- preact/Music.tsx
+++ preact/Music.tsx
@@ -7,34 +7,13 @@ import youTubePlayer from 'youtube-player';
 
 const styled = scoped(h);
 
-const thumbs = [
-  'https://i.ytimg.com/vi/jis7E_mbwPw/hqdefault.jpg?sqp=-oaymwEYCKgBEF5IVfKriqkDCwgBFQAAiEIYAXAB&rs=AOn4CLDhHYwioPk-hofibrZgnyP3gQA7Pw',
-  'https://i.ytimg.com/vi/8q-Ny2a0GPM/hqdefault.jpg?sqp=-oaymwEYCKgBEF5IVfKriqkDCwgBFQAAiEIYAXAB&rs=AOn4CLDGolpXKf5loD2lBDHtcnES9iNFCQ',
-  'https://i.ytimg.com/vi/FkyotXkl-CI/hqdefault.jpg?sqp=-oaymwEYCKgBEF5IVfKriqkDCwgBFQAAiEIYAXAB&rs=AOn4CLBjgfB89PAmOqOQt0JjGg5_37iQoQ',
-  'https://i.ytimg.com/vi/owl2ZtgDRHI/hqdefault.jpg?sqp=-oaymwEYCKgBEF5IVfKriqkDCwgBFQAAiEIYAXAB&rs=AOn4CLD0tcHy7wZ-zKyzx-Y7tKwT08BCYA',
-  'https://i.ytimg.com/vi/-YLyVDOJnFc/hqdefault.jpg?sqp=-oaymwEYCKgBEF5IVfKriqkDCwgBFQAAiEIYAXAB&rs=AOn4CLApDrPcstG4CqzdMG7hTyQNfzNLpg',
-  'https://i.ytimg.com/vi/3JAeD-9FE8I/hqdefault.jpg?sqp=-oaymwEYCKgBEF5IVfKriqkDCwgBFQAAiEIYAXAB&rs=AOn4CLA8x6yUUIJtL9ZlHSxvOFwIBjUsDw',
-  'https://i.ytimg.com/vi/KQPr8Qde22w/hqdefault.jpg?sqp=-oaymwEYCKgBEF5IVfKriqkDCwgBFQAAiEIYAXAB&rs=AOn4CLBjH8nBGhQlFL2ydqyJuEe0NBJ_7A',
-  'https://i.ytimg.com/vi/IfYODh82hTo/hqdefault.jpg?sqp=-oaymwEYCKgBEF5IVfKriqkDCwgBFQAAiEIYAXAB&rs=AOn4CLDLrSuU4eehrghGgAVZDk-cXuYbqg',
-  'https://i.ytimg.com/vi/abDwOIMcBEw/hqdefault.jpg?sqp=-oaymwEYCKgBEF5IVfKriqkDCwgBFQAAiEIYAXAB&rs=AOn4CLC0KlSAnzL_nktfx7qf7KiVCHgERg',
-  'https://i.ytimg.com/vi/gXhqIT7XnsI/hqdefault.jpg?sqp=-oaymwEYCKgBEF5IVfKriqkDCwgBFQAAiEIYAXAB&rs=AOn4CLBxUvSYmAYG8ei-qCtF5aqf1S2jLg',
-  'https://i.ytimg.com/vi/VBpXOOPK6-E/hqdefault.jpg?sqp=-oaymwEYCKgBEF5IVfKriqkDCwgBFQAAiEIYAXAB&rs=AOn4CLAcElqEouylVK7a274rD5bRPnlhpA',
-  'https://i.ytimg.com/vi/_AcOHoF68vo/hqdefault.jpg?sqp=-oaymwEYCKgBEF5IVfKriqkDCwgBFQAAiEIYAXAB&rs=AOn4CLDeJGN6g9KEQsiyh0eLw29aKjq65Q',
-  'https://i.ytimg.com/vi/s9IvNnu0kqc/hqdefault.jpg?sqp=-oaymwEYCKgBEF5IVfKriqkDCwgBFQAAiEIYAXAB&rs=AOn4CLAfjZkueF2Vl9L7GJqWfBYXLpaUMA',
-  'https://i.ytimg.com/vi/FVfim9jNQVg/hqdefault.jpg?sqp=-oaymwEYCKgBEF5IVfKriqkDCwgBFQAAiEIYAXAB&rs=AOn4CLCHSMjzh5CXh8yoZ97PjjYK_710yg',
-  'https://i.ytimg.com/vi/sYNL6WlW-e8/hqdefault.jpg?sqp=-oaymwEYCKgBEF5IVfKriqkDCwgBFQAAiEIYAXAB&rs=AOn4CLDqlwkXtOtcqWR5aSY6AvEM2qpqZA',
-  'https://i.ytimg.com/vi/pSBk3QVY3cA/hqdefault.jpg?sqp=-oaymwEYCKgBEF5IVfKriqkDCwgBFQAAiEIYAXAB&rs=AOn4CLAFgQVWgVeexxg56nvSdUh3F1pAxQ',
-  'https://i.ytimg.com/vi/b0nF7oUbsfg/hqdefault.jpg?sqp=-oaymwEYCKgBEF5IVfKriqkDCwgBFQAAiEIYAXAB&rs=AOn4CLBwwRzQaO2eRKNpPuxZAE3FrLGDrQ',
-  'https://i.ytimg.com/vi/9Ppv1wvwHXI/hqdefault.jpg?sqp=-oaymwEYCKgBEF5IVfKriqkDCwgBFQAAiEIYAXAB&rs=AOn4CLC3s7BDFQoyRVLG70rB5H9IglQW4A',
-  'https://i.ytimg.com/vi/58al_aq_E2M/hqdefault.jpg?sqp=-oaymwEYCKgBEF5IVfKriqkDCwgBFQAAiEIYAXAB&rs=AOn4CLAqQGYzz8issfnlAXWpd4xPSImxZg',
-  'https://i.ytimg.com/vi/XAt2lODxz_E/hqdefault.jpg?sqp=-oaymwEYCKgBEF5IVfKriqkDCwgBFQAAiEIYAXAB&rs=AOn4CLDgKjhUDjVllfbHbVi9ScF496cuYw',
-  'https://i.ytimg.com/vi/58TGnNnXT6E/hqdefault.jpg?sqp=-oaymwEYCKgBEF5IVfKriqkDCwgBFQAAiEIYAXAB&rs=AOn4CLD36841bBz6htYcX_bJr_Ii_xmpRg',
-];
-
-const MusicPlayer: FunctionalComponent = () => {
+const MusicPlayer: FunctionalComponent<{thumbs: string[]}> = ({thumbs}) => {
   const divRef = useRef<HTMLDivElement>();
   const youtubeRef = useRef<ReturnType<typeof youTubePlayer>>();
-  const [playlist, setPlaylist] = useState(thumbs);
+  const [playlist, setPlaylist] = useState([
+    thumbs[thumbs.length - 1],
+    ...thumbs.slice(0, thumbs.length - 2),
+  ]);
 
   const [move, setMove] = useState(0);
 
@@ -42,7 +21,7 @@ const MusicPlayer: FunctionalComponent = () => {
     (youtubeRef as any).current = youTubePlayer(divRef.current!, {
       width: window.innerWidth < 500 ? window.innerWidth - 100 : 500,
     });
-    setMusic(thumbs[0]);
+    setMusic(playlist[1]);
   }, []);
 
   const setMusic = (url: string) => {
@@ -55,22 +34,26 @@ const MusicPlayer: FunctionalComponent = () => {
   };
 
   const right = () => {
-    setMove(200);
-    setTimeout(() => setMove(0), 0);
-
     const arr = [...playlist];
     const shift = arr.shift();
     setPlaylist([...arr, shift!]);
+
+    setMove(200);
+    setTimeout(() => setMove(0), 0);
   };
 
   const left = () => {
-    setMove(-200);
-    setTimeout(() => setMove(0), 0);
-
     const arr = [...playlist];
     const pop = arr.pop();
     setPlaylist([pop!, ...arr]);
+
+    setMove(-200);
+    setTimeout(() => setMove(0), 0);
   };
+
+  const leftEnd = [...playlist].pop()!;
+
+  console.log(move);
 
   return (
     <div>
@@ -92,9 +75,17 @@ const MusicPlayer: FunctionalComponent = () => {
         }}>
         <Button className="fas fa-arrow-left" onClick={left} />
         <List>
-          {playlist.map((url, i) => (
-            <Card src={url} key={i} onClick={() => setMusic(url)} move={move} />
-          ))}
+          {playlist.map(
+            (url, i) =>
+              i < playlist.length && (
+                <Card
+                  src={url}
+                  key={i}
+                  onClick={() => setMusic(url)}
+                  move={move}
+                />
+              ),
+          )}
         </List>
         <Button
           className="fas fa-arrow-right"
@@ -138,7 +129,6 @@ const Card = styled('img')`
   margin: 10px 5px;
   width: 200px;
 
-  transition: ${(props: any) =>
-    props.move === 0 ? `all 0.3s ease-out` : 'none'};
-  transform: ${(props: any) => `translateX(${props.move}px)`};
+  transition: ${(props: any) => (props.move === 0 ? `all .4s` : 'none')};
+  transform: ${(props: any) => `translateX(${props.move - 180}px)`};
 `;
