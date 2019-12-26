8122b584e7e90f9aa1185d889685f90f70b60e24
waradin <kuajiyoshi@gmail.com>
2019-12-26 20:36:06 +0900

    質問追加分をビルド

--- preact_build/sindanQuestions.js
+++ preact_build/sindanQuestions.js
@@ -30,11 +30,61 @@ var data = {
                 },
                 {
                     text: "いいえ",
-                    target: "none"
+                    target: "none",
+                    value: 0
+                }
+            ]
+        },
+        {
+            text: "好きな食べ物は何ですか？",
+            answers: [
+                {
+                    text: "VRゴーグル",
+                    target: "ray",
+                    value: 2
+                },
+                {
+                    text: "水",
+                    target: "unchan",
+                    value: 2
+                },
+                {
+                    text: "マネーーー！！！",
+                    target: "rio",
+                    value: 2
+                },
+                {
+                    text: "ハンバーガー",
+                    target: "none",
+                    value: 0
+                }
+            ]
+        },
+        {
+            text: "墓地にはEXデッキから直接送られた「PSYフレームロード・Ω」が存在します。どうしますか？",
+            answers: [
+                {
+                    text: "死者蘇生を発動",
+                    target: "rio",
+                    value: 1
+                },
+                {
+                    text: "リビングデットの呼び声を発動",
+                    target: "unchan",
+                    value: 1
+                },
+                {
+                    text: "カードを売ってマネーにする",
+                    target: "rio",
+                    value: 2
+                },
+                {
+                    text: "何もせずターンエンド",
+                    target: "ray",
+                    value: 2
                 }
             ]
         }
     ]
 };
-//const data :string= "1";
 export default data;
