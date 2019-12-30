d6ff7b9b7e4333f258509f946032ce355f355f5d
s4na <appletea.umauma@gmail.com>
2019-12-31 07:23:27 +0900

    change from cookie to local storage

--- assets/js/lucky_rayrio.js
+++ assets/js/lucky_rayrio.js
@@ -1,20 +1,16 @@
 const luckyRayRio = () => {
-  var isSiteVisit = document.cookie.replace(
-    /(?:(?:^|.*;\s*)is-site-visit\s*\=\s*([^;]*).*$)|^.*$/,
-    '$1',
-  );
-
+  const isSiteVisit = localStorage.getItem('isSiteVisit');
   if (isSiteVisit === 'true') {
     const probability = 1.0;
-
     const isLucky = Math.random() * 100 < probability;
     const toAddClass = isLucky ? 'lucky' : 'normal';
 
     for (const element of document.getElementsByClassName('links')) {
       element.classList.add(toAddClass);
     }
+  } else {
+    localStorage.setItem('isSiteVisit', 'true');
   }
-  document.cookie = 'is-site-visit=true';
 };
 
 luckyRayRio();
