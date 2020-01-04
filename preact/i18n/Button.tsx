8c04967028cc45add8df69b6a80d65166291399f
shinyoshiaki <shinyoshiaki2012@gmail.com>
2020-01-04 19:50:16 +0900

    add icon

--- preact/i18n/Button.tsx
+++ preact/i18n/Button.tsx
@@ -1,16 +1,31 @@
 import {FunctionalComponent, h} from 'preact';
 
+// @ts-ignore
+import scoped from 'scoped-style';
+import {useCallback} from 'preact/hooks';
 import {useI18n} from '../hooks/i18n';
+
+const styled = scoped(h);
 
 const Button: FunctionalComponent = () => {
   const i18n = useI18n();
+  const setLanguage = useCallback(
+    (s: string) => (_: any) => i18n.setLanguage(s),
+    [i18n],
+  );
 
   return (
     <div style={{display: 'flex'}}>
-      <button>ja</button>
-      <button onClick={() => i18n.setLanguage('en')}>en</button>
+      <ClickSpan class="flag-icon flag-icon-jp" onClick={setLanguage('ja')} />
+      <ClickSpan class="flag-icon flag-icon-gb" onClick={setLanguage('en')} />
+      <ClickSpan class="flag-icon flag-icon-cn" onClick={setLanguage('cn')} />
     </div>
   );
 };
+
+const ClickSpan = styled('span')`
+  cursor: pointer;
+  margin: 3px;
+`;
 
 export default Button;
