e033bd9f7fed0b32fde191a6821a9052377fd7bc
ryamaguchi0220 <maguchi579@gmail.com>
2020-01-21 19:10:39 +0900

    chore: remove unnecessary codes

--- preact/i18n/Switcher.tsx
+++ preact/i18n/Switcher.tsx
@@ -2,10 +2,6 @@ import {Fragment, FunctionalComponent, h} from 'preact';
 import {useCallback, useEffect, useRef, useState} from 'preact/hooks';
 
 import I18n from './i18n';
-// @ts-ignore
-import scoped from 'scoped-style';
-
-const styled = scoped(h);
 
 const Switcher: FunctionalComponent = () => {
   const [select, setSelect] = useState('ja');
@@ -39,11 +35,5 @@ const Switcher: FunctionalComponent = () => {
     </Fragment>
   );
 };
-
-const ClickSpan = styled('span')`
-  cursor: ${({select}: any) => (select ? '' : 'pointer')};
-  margin: 3px;
-  opacity: ${({select}: any) => (select ? 1 : 0.4)};
-`;
 
 export default Switcher;
